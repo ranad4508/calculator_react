14186178e574f7fce1fb2ff18c98ede01ddcf40a
Dinesh Rana <ranad4508@gmail.com>
2024-03-29 18:25:31 +0545

    useState maintained

--- react-10-projects/react_hooks/src/App.jsx
+++ react-10-projects/react_hooks/src/App.jsx
@@ -1,40 +1,11 @@
-import { useState } from "react";
+import UseState from "./hooks/UseState";
 
 function App() {
-  const [state, setState] = useState({ count: 4, theme: "blue" });
-  const [themes, setThemes] = useState('Red');
-  const count = state.count;
-  const theme = state.theme;
-
-  function incrementCount() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        count: prevState.count + 1,
-      };
-    });
-  }
-  function decrementCount() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        count: prevState.count - 1,
-        
-      };
-      
-    });
-    setThemes('Green')
-  }
-
   return (
     <>
       <div>
-        <button onClick={decrementCount}>-</button>
-        <span>{themes}</span>
-        <span>{count}</span>
-        <span>{theme}</span>
-        <button onClick={incrementCount}>+</button>
-      </div>
+        <UseState />
+      </div>{" "}
     </>
   );
 }
