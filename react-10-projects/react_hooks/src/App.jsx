fe2496f92778f3c53dedba1155cd9fbb6d810dee
Dinesh Rana <ranad4508@gmail.com>
2024-03-30 12:59:22 +0545

    useEffect completed

--- react-10-projects/react_hooks/src/App.jsx
+++ react-10-projects/react_hooks/src/App.jsx
@@ -1,10 +1,14 @@
+import UseEffect from "./hooks/UseEffect";
 import UseState from "./hooks/UseState";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        <UseState />
+        <h2> useState Hook </h2> <UseState />
+        <br />
+        <h2> useEffect Hook </h2> <UseEffect />
       </div>{" "}
     </>
   );
